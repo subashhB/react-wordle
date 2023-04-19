a16b26adb03df92c79b72595f70c7d8e74c46133
Admin <subashbaidya9@gmail.com>
2023-04-19 09:42:24 +0545

    JSON Server and fetching a random word

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const[ solution, setSolution] = useState(null);
+  useEffect(()=>{
+    fetch(' http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json =>{
+        //Random Solution from the JSON server
+        const randomSolution = json[Math.floor(Math.random()*json.length)];
+        setSolution(randomSolution.word);
+      })
+  },[])
   return (
     <div className="App">
       <h1>Wordle</h1>
+      {solution && (<div>Solution is: {solution}</div>)}
     </div>
   );
 }
